7a3c5f1e490b7cd1e4539861484db9f9c8b00588
Ian Henshaw <ian@thehenshaws.net>
2018-05-28 09:04:58 +0100

    allow devServer override to return a Promise for the configuration

--- packages/react-app-rewired/scripts/start.js
+++ packages/react-app-rewired/scripts/start.js
@@ -12,8 +12,13 @@ const devServerConfig = require(devServerConfigPath);
 require.cache[require.resolve(webpackConfigPath)].exports =
   overrides.webpack(webpackConfig, process.env.NODE_ENV);
 
-require.cache[require.resolve(devServerConfigPath)].exports =
-  overrides.devServer(devServerConfig, process.env.NODE_ENV);
+// we may wish to defer the start process while we wait for the dev-server config to be created, so use
+// Promise.resolve to enable `overrides.devServer` to return a Promise for the config if desired.
+const devServerOverrides = overrides.devServer(devServerConfig, process.env.NODE_ENV);
+Promise.resolve(devServerOverrides)
+    .then((devServerOverrides) => {
+        require.cache[require.resolve(devServerConfigPath)].exports =
 
-// run original script
-require(paths.scriptVersion + "/scripts/start");
+        // run original script
+        require(paths.scriptVersion + "/scripts/start");
+    });
